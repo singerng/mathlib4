e69fe78c8bdf6ccd813a8f7ef8b729baf6181854
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2024-10-12 09:32:25 +0200

    added toFinset_eq

--- Mathlib/Data/Sym/Sym2.lean
+++ Mathlib/Data/Sym/Sym2.lean
@@ -7,6 +7,7 @@ import Mathlib.Data.Finset.Prod
 import Mathlib.Data.Sym.Basic
 import Mathlib.Data.Sym.Sym2.Init
 import Mathlib.Data.SetLike.Basic
+import Mathlib.Data.Fintype.Basic
 
 /-!
 # The symmetric square
@@ -639,6 +640,9 @@ instance : Coe (Sym2 α) (Finset α) := ⟨Sym2.toFinset⟩
   induction z; simp
 
 @[simp] lemma coe_toFinset {z : Sym2 α} : ((z : Finset α) : Set α) = z := by
+  ext; simp
+
+@[simp] lemma toFinset_eq [Fintype α] {e : Sym2 α} : (e : Finset α) = {v | v ∈ e}.toFinset := by
   ext; simp
 
 lemma card_toFinset_of_isDiag {z : Sym2 α} (h : z.IsDiag) : (z : Finset α).card = 1 := by
