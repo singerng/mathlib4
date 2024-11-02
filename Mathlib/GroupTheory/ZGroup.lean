1243a9e594dddcf49e4a2debb07257b3c486b57e
Thomas Browning <tb65536@uw.edu>
2024-11-01 23:39:18 -0700

    Burnside progress

--- Mathlib/GroupTheory/ZGroup.lean
+++ Mathlib/GroupTheory/ZGroup.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Thomas Browning. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
+import Mathlib.Algebra.Ring.AddAut
 import Mathlib.GroupTheory.SchurZassenhaus
 import Mathlib.GroupTheory.Transfer
 import Mathlib.NumberTheory.Padics.PadicVal.Basic
@@ -94,6 +95,109 @@ theorem Sylow.mapSurjective_surjective {G G' : Type*} [Group G] [Finite G] [Grou
   rw [IsPGroup.toSylow_coe, Subgroup.map_le_iff_le_comap]
   exact Subgroup.map_subtype_le Q.1
 
+section Burnside
+
+def Subgroup.autmap {G : Type*} [Group G] (H : Subgroup G) : H.normalizer →* MulAut H where
+  toFun := fun g ↦
+    { toFun := fun h ↦ ⟨g * h * g⁻¹, (g.2 h).mp h.2⟩
+      invFun := fun h ↦ ⟨g⁻¹ * h * g, (mem_normalizer_iff''.mp g.2 h).mp h.2⟩
+      left_inv := fun h ↦ by simp [mul_assoc]
+      right_inv := fun h ↦ by simp [mul_assoc]
+      map_mul' := fun h1 h2 ↦ by simp [mul_assoc] }
+  map_one' := by simp [DFunLike.ext_iff]
+  map_mul' := by simp [DFunLike.ext_iff, mul_assoc]
+
+theorem Subgroup.autmap_ker {G : Type*} [Group G] (H : Subgroup G) :
+    H.autmap.ker = (Subgroup.centralizer H).subgroupOf H.normalizer := by
+  ext g
+  simp [DFunLike.ext_iff, mem_subgroupOf, autmap, mem_centralizer_iff, mul_inv_eq_iff_eq_mul]
+  simp [eq_comm]
+
+#eval Nat.minFac 1
+
+def MulAut.congr {G H : Type*} [Group G] [Group H] (ϕ : G ≃* H) :
+    MulAut G ≃* MulAut H where
+  toFun := fun f ↦ ϕ.symm.trans (f.trans ϕ)
+  invFun := fun f ↦ ϕ.trans (f.trans ϕ.symm)
+  left_inv _ := by simp [DFunLike.ext_iff]
+  right_inv _ := by simp [DFunLike.ext_iff]
+  map_mul' := by simp [DFunLike.ext_iff]
+
+def MulAut.ofMultiplicative (A : Type*) [AddGroup A] : MulAut (Multiplicative A) ≃* AddAut A where
+  toFun := sorry
+  invFun := sorry
+  left_inv := sorry
+  right_inv := sorry
+  map_mul' := sorry
+
+def ringequiv (n : ℕ) : AddAut (ZMod n) ≃* (ZMod n)ˣ := sorry
+
+theorem IsCyclic.card_mulAut {G : Type*} [Group G] [Finite G] [h : IsCyclic G] :
+    Nat.card (MulAut G) = Nat.totient (Nat.card G) := by
+  have : NeZero (Nat.card G) := ⟨Nat.card_pos.ne'⟩
+  rw [← ZMod.card_units_eq_totient, ← Nat.card_eq_fintype_card]
+  have key := MulAut.congr (zmodCyclicMulEquiv h)
+  have key2 := MulAut.ofMultiplicative (ZMod (Nat.card G))
+  have key3 := key.symm.trans (key2.trans (ringequiv (Nat.card G)))
+  exact Nat.card_congr key3
+
+theorem thm1 {G : Type*} [Group G] [Finite G] {P : Sylow (Nat.card G).minFac G} (hP : IsCyclic P) :
+    P.normalizer ≤ Subgroup.centralizer (P : Set G) := by
+  by_cases hn : Nat.card G = 1
+  · have := (Nat.card_eq_one_iff_unique.mp hn).1
+    rw [Subsingleton.elim P.normalizer ⊥]
+    exact bot_le
+  have key := Subgroup.card_dvd_of_injective _ (QuotientGroup.kerLift_injective P.autmap)
+  rw [Subgroup.autmap_ker, ← Subgroup.index, ← Subgroup.relindex] at key
+  refine Subgroup.relindex_eq_one.mp (Nat.eq_one_of_dvd_coprimes ?_ dvd_rfl key)
+  have := Fact.mk (Nat.minFac_prime hn)
+  obtain ⟨k, hk⟩ := P.2.exists_card_eq
+  rcases eq_zero_or_pos k with h0 | h0
+  · rw [hP.card_mulAut, hk, h0, pow_zero, Nat.totient_one]
+    apply Nat.coprime_one_right
+  rw [hP.card_mulAut, hk, Nat.totient_prime_pow (Nat.minFac_prime hn) h0,
+    Nat.coprime_mul_iff_right]
+  refine ⟨Nat.Coprime.pow_right _ ?_, ?_⟩
+  · have key' : P.IsCommutative := by
+      let h := hP.commGroup
+      exact ⟨⟨CommGroup.mul_comm⟩⟩
+    have key := P.le_centralizer
+    replace key := Subgroup.relindex_dvd_of_le_left P.normalizer key
+    apply Nat.Coprime.coprime_dvd_left key
+    replace key := Subgroup.relindex_dvd_index_of_le P.le_normalizer
+    apply Nat.Coprime.coprime_dvd_left key
+    rw [Nat.coprime_comm, Nat.Prime.coprime_iff_not_dvd (Nat.minFac_prime hn)]
+    apply not_dvd_index_sylow P
+    exact Nat.card_pos.ne'
+  · rw [Nat.Coprime]
+    have h1 :=
+      Nat.gcd_dvd_left ((Subgroup.centralizer P).relindex P.normalizer) ((Nat.card G).minFac - 1)
+    replace h1 : ((Subgroup.centralizer P).relindex P.normalizer).gcd ((Nat.card G).minFac - 1)
+      ∣ Nat.card G := by
+      refine h1.trans ?_
+      rw [← Subgroup.inf_relindex_left]
+      exact (Subgroup.relindex_dvd_index_of_le inf_le_left).trans (Subgroup.index_dvd_card _)
+    have h2 := Nat.gcd_le_right
+      (m := (Subgroup.centralizer P).relindex P.normalizer) ((Nat.card G).minFac - 1)
+      (by simp only [tsub_pos_iff_lt, (Nat.minFac_prime hn).one_lt])
+    contrapose! h2
+    · refine Nat.sub_one_lt_of_le (Nat.card G).minFac_pos ?_
+      refine Nat.minFac_le_of_dvd ?_ h1
+      rw [Nat.two_le_iff]
+      exact ⟨ne_zero_of_dvd_ne_zero Nat.card_pos.ne' h1, h2⟩
+
+theorem thm2 {G : Type*} [Group G] [Finite G] (P : Sylow (Nat.card G).minFac G) (hP : IsCyclic P) :
+    (MonoidHom.transferSylow P (thm1 hP)).ker.IsComplement' P := by
+  by_cases hn : Nat.card G = 1
+  · have := (Nat.card_eq_one_iff_unique.mp hn).1
+    rw [Subsingleton.elim (MonoidHom.transferSylow P (thm1 hP)).ker ⊥]
+    rw [Subsingleton.elim P.1 ⊤]
+    exact Subgroup.isComplement'_bot_top
+  have := Fact.mk (Nat.minFac_prime hn)
+  exact MonoidHom.ker_transferSylow_isComplement' P (thm1 hP)
+
+end Burnside
+
 variable (G G' : Type*) [Group G] [Group G'] (f : G →* G')
 
 class IsZGroup : Prop where
@@ -122,6 +226,8 @@ theorem of_surjective [Finite G] (hG' : IsZGroup G) (hf : Function.Surjective f)
   obtain ⟨Q, rfl⟩ := Sylow.mapSurjective_surjective hf p P
   specialize hG' p hp Q
   exact isCyclic_of_surjective _ (f.subgroupMap_surjective Q)
+
+
 
 -- If smallest prime divisor is cyclic, then G has normal p-complement
 -- ZGroup implies solvable: Induct
