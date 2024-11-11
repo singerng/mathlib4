d8ee0ea6210f041a77eca35c745ae54fcac0c973
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-11-11 16:24:33 +0100

    wip

--- Mathlib/Sandox.lean
+++ Mathlib/Sandox.lean
@@ -2,8 +2,6 @@ import Mathlib
 
 open NNReal
 
-variable (F : ℕ → ℝ≥0) (hF : ∀ r, {n | F n ≤ r}.Finite)
+variable (F : ℕ → ℝ) (hF : ∀ r, {n | F n ≤ r}.Finite)
 
 example : ∃ e : ℕ → ℕ, Monotone (F ∘ e) := sorry
-
-
